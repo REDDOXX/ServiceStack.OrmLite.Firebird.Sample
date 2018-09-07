0030ca20189c0b99bfbbcd18cd8b77de8effd6ee
Alexander Bauer <alexander.bauer@reddoxx.com>
2018-09-07 15:54:12 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ServiceStack.OrmLite.Firebird.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =
+                "User=SYSDBA;" +
+                "Password=secret;" +
+                "Database=/tmp/database.fdb;" +
+                "DataSource=localhost;" +
+                "Port=3050;" +
+                "Dialect=3;" +
+                "Charset=ISO8859_1;" +
+                "Role=;" +
+                "Connection lifetime=15;" +
+                "Pooling=true;" +
+                "MinPoolSize=0;" +
+                "MaxPoolSize=50;" +
+                "Packet Size=8192;" +
+                "ServerType=0";
+
+            var dbFactory = new OrmLiteConnectionFactory(
+                connectionString,
+                FirebirdDialect.Provider);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# ServiceStack.OrmLite.Firebird.Sample
